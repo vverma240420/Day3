42fe93f6f6fc4152fcc462d0eeb79d9e272714e5
Vivek Verma <vverma240420@gmail.com>
2022-12-25 18:02:32 +0530

    First commit

---
+++ addition.sh
@@ -0,0 +1,7 @@
+read -p "A:" a
+read -p "B:" b
+echo $a
+echo $b
+C= $(($a +$b))
+echo $Z
+
---
+++ if.sh
@@ -0,0 +1,7 @@
+read -p "Enter the number: " a
+read -p "Enter the number: " b
+if [ $a -lt $b ]
+then
+echo $a is less than $b
+fi
+
